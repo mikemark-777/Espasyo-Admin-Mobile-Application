88ded042a1b6a3d243d3cc5d00f3749619e7c664
mikemark-777 <65594383+mikemark-777@users.noreply.github.com>
2021-11-07 21:39:56 +0800

    added images to cardviews

--- app/src/main/java/com/capstone/espasyoadmin/admin/AdminMainActivity.java
+++ app/src/main/java/com/capstone/espasyoadmin/admin/AdminMainActivity.java
@@ -1,16 +1,46 @@
 package com.capstone.espasyoadmin.admin;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.capstone.espasyoadmin.R;
+import com.capstone.espasyoadmin.admin.views.PropertyMasterListActivity;
 
 public class AdminMainActivity extends AppCompatActivity {
+
+    private CardView btnGotoPropertyMasterList,
+                     btnGotoPropertyOnMap,
+                     btnGotoVerificationRequests;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.admin_activity__main);
+
+        initializeViews();
+
+        btnGotoPropertyMasterList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gotoPropertyMasterList();
+            }
+        });
+
+    }
+
+    public void initializeViews() {
+        btnGotoPropertyMasterList = findViewById(R.id.btnGotoPropertyMasterlist);
+        btnGotoPropertyOnMap = findViewById(R.id.btnGotoPropertyOnMap);
+        btnGotoVerificationRequests = findViewById(R.id.btnGotoVerificaitonRequests);
+    }
+
+    public void gotoPropertyMasterList() {
+        Intent intent = new Intent(AdminMainActivity.this, PropertyMasterListActivity.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
     }
 }
---
+++ app/src/main/java/com/capstone/espasyoadmin/admin/views/PropertyMasterListActivity.java
@@ -0,0 +1,16 @@
+package com.capstone.espasyoadmin.admin.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.capstone.espasyoadmin.R;
+
+public class PropertyMasterListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.admin_activity_property_master_list);
+    }
+}
