bcb5f4a2257250b41a7b5f9e1d475da89da16f40
mikemark-777 <65594383+mikemark-777@users.noreply.github.com>
2021-11-08 00:34:39 +0800

    edited uis

--- app/src/main/java/com/capstone/espasyoadmin/admin/AdminMainActivity.java
+++ app/src/main/java/com/capstone/espasyoadmin/admin/AdminMainActivity.java
@@ -6,15 +6,19 @@ import androidx.cardview.widget.CardView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.capstone.espasyoadmin.R;
 import com.capstone.espasyoadmin.admin.views.PropertyMasterListActivity;
 
 public class AdminMainActivity extends AppCompatActivity {
 
+    private ImageView btnIconGotoProfile;
+
     private CardView btnGotoPropertyMasterList,
                      btnGotoPropertyOnMap,
                      btnGotoVerificationRequests;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,9 +34,20 @@ public class AdminMainActivity extends AppCompatActivity {
             }
         });
 
+        btnIconGotoProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gotoPropertyMasterList();
+            }
+        });
+
     }
 
     public void initializeViews() {
+        //imageViews
+        btnIconGotoProfile = findViewById(R.id.btnIconGotoProfile);
+
+        //cardViews
         btnGotoPropertyMasterList = findViewById(R.id.btnGotoPropertyMasterlist);
         btnGotoPropertyOnMap = findViewById(R.id.btnGotoPropertyOnMap);
         btnGotoVerificationRequests = findViewById(R.id.btnGotoVerificaitonRequests);
